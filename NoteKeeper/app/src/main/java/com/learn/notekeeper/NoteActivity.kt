75fb37c090d2c662eba54aa51d1e756d6044f4f1
mohammad <shalabi67@hotmail.com>
2017-11-05 05:17:55 +0100

    arrange code

--- NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteActivity.kt
+++ NoteKeeper/app/src/main/java/com/learn/notekeeper/NoteActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -20,7 +19,6 @@ import com.learn.notekeeper.data.note.Note
 import com.learn.notekeeper.data.note.Notes
 
 import kotlinx.android.synthetic.main.activity_note.*
-import java.net.URL
 
 class NoteActivity : AppCompatActivity() {
     companion object {
@@ -36,7 +34,7 @@ class NoteActivity : AppCompatActivity() {
     lateinit private var oldNote:Note
 
 
-    var isSave = true
+    private var isSave = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note)
@@ -88,7 +86,7 @@ class NoteActivity : AppCompatActivity() {
         textView = findViewById<TextView>(R.id.text_note_text)
         textView.text = note.noteText
 
-        imageView.setImageBitmap(note?.image)
+        imageView.setImageBitmap(note.image)
 
         val spinnerView = findViewById<Spinner>(R.id.spinner_courses)
         val index = Courses.courses.indexOfFirst { course -> course.courseTitle == note.course?.courseTitle }
@@ -133,6 +131,22 @@ class NoteActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+    private fun sendEmail() : Boolean {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc2822"
+        intent.putExtra(Intent.EXTRA_SUBJECT, titleView.text.toString())
+        intent.putExtra(Intent.EXTRA_TEXT, "Shalabi Note: ${textView.text.toString()}")
+
+        try {
+            startActivity(intent)
+        } catch(e : Exception) {
+            Snackbar.make(titleView, "Could not send email. ${e.message}", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        return true
+    }
+
 
     private fun getImageFromCamera(): Boolean {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -152,23 +166,6 @@ class NoteActivity : AppCompatActivity() {
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
-
-    private fun sendEmail() : Boolean {
-        val intent = Intent(Intent.ACTION_SEND)
-        intent.type = "message/rfc2822"
-        intent.putExtra(Intent.EXTRA_SUBJECT, titleView.text.toString())
-        intent.putExtra(Intent.EXTRA_TEXT, "Shalabi Note: ${textView.text.toString()}")
-
-        try {
-            startActivity(intent)
-        } catch(e : Exception) {
-            Snackbar.make(titleView, "Could not send email. ${e.message}", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-
-        return true
-    }
-
     override fun onPause() {
         super.onPause()
 
