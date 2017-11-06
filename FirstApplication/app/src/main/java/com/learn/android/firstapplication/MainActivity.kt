c6cf05e782b908663595f393362cda57a6c16e2b
mshalabi <mohamad.shalabi@mytoys.de>
2017-11-06 08:19:55 +0100

    Adding FirstApplication

---
+++ FirstApplication/app/src/main/java/com/learn/android/firstapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.learn.android.firstapplication
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val textView:TextView = findViewById(R.id.text_value)
+            val number = textView.text.toString().toInt()
+            val newNumber = number + number
+            textView.text = newNumber.toString()
+
+            Snackbar.make(view, "changed $number to $newNumber", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
