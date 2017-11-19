7a58a591959a24ba33cdd5a507eec186407f8f2c
mohammad <shalabi67@hotmail.com>
2017-11-19 18:10:02 +0100

    setting: adding setting activity.

--- NoteKeeper/app/src/main/java/com/learn/notekeeper/MainActivity.kt
+++ NoteKeeper/app/src/main/java/com/learn/notekeeper/MainActivity.kt
@@ -106,7 +106,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.action_settings -> return true
+            R.id.action_settings ->  {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                return true
+            }
             else -> return super.onOptionsItemSelected(item)
         }
     }
