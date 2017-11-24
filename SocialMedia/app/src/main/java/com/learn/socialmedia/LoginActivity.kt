db79702154e226f0856d9209d8fae65a9193f9c2
mohammad <shalabi67@hotmail.com>
2017-11-24 22:54:51 +0100

    Twitter login button worked

--- SocialMedia/app/src/main/java/com/learn/socialmedia/LoginActivity.kt
+++ SocialMedia/app/src/main/java/com/learn/socialmedia/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.learn.socialmedia
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.util.Log
+import android.view.View
 import com.facebook.CallbackManager
 import com.facebook.FacebookSdk
 import com.facebook.login.widget.LoginButton
@@ -11,7 +13,13 @@ import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
 import com.facebook.FacebookCallback
+import com.twitter.sdk.android.core.*
 import java.util.concurrent.Callable
+import com.twitter.sdk.android.core.identity.TwitterLoginButton
+import com.twitter.sdk.android.core.TwitterException
+import com.twitter.sdk.android.core.identity.TwitterAuthClient
+
+
 
 
 class LoginActivity : AppCompatActivity() {
@@ -21,6 +29,8 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var  facebookSignInButton : LoginButton
     lateinit var facebookCallbackManager : CallbackManager
+    lateinit var twitterLoginButton : TwitterLoginButton
+    var twitterSession : TwitterSession? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -54,17 +64,61 @@ class LoginActivity : AppCompatActivity() {
 
         )
 
+        twitterLoginButton = findViewById<TwitterLoginButton>(R.id.login_button)
+        twitterLoginButton.callback = object : Callback<TwitterSession>() {
+            override fun success(result: Result<TwitterSession>) {
+                twitterSession = TwitterCore.getInstance().sessionManager.activeSession
+                val session = twitterSession?:return
+                val authToken = session.authToken
+                val token = authToken.token
+                val secret = authToken.secret
+                Log.d(TAG, "Token = $token")
+                Log.d(TAG, "Secret = $secret")
 
+                //get email
+                getTwitterEmail()
+            }
+
+            override fun failure(exception: TwitterException) {
+                Log.e(TAG, "Twitter login failed.")
+                Log.e(TAG, exception.message)
+
+                Snackbar.make(twitterLoginButton, "Twitter login failed", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show()
+            }
+        }
+
+
+    }
+    var userEmail  = ""
+    private fun getTwitterEmail()  {
+        val session = twitterSession?:return
+        val authClient = TwitterAuthClient()
+        authClient.requestEmail(session, object : Callback<String>() {
+            override fun success(result: Result<String>) {
+                val email = result.data?:return
+                Log.d(TAG, email)
+                userEmail = email
+            }
+
+            override fun failure(exception: TwitterException) {
+                userEmail = ""
+            }
+        })
     }
 
     private fun handleSignInResult(o: Any?) {
-        //Handle sign result here
+        Log.d(TAG, "handleSignInResult")
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        facebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 140) {
+            twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+        } else {
+            facebookCallbackManager.onActivityResult(requestCode, resultCode, data);
+        }
 
     }
 }
---
+++ SocialMedia/app/src/main/java/com/learn/socialmedia/SocialMediaApplication.kt
@@ -0,0 +1,26 @@
+package com.learn.socialmedia
+
+import android.app.Application
+import android.util.Log
+import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.DefaultLogger
+import com.twitter.sdk.android.core.TwitterConfig
+
+
+
+/**
+ * Created by mohammad on 11/24/2017.
+ */
+class SocialMediaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val config = TwitterConfig.Builder(this)
+                .logger(DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(TwitterAuthConfig(getString(R.string.CONSUMER_KEY),
+                        getString(R.string.CONSUMER_SECRET)))
+                .debug(true)
+                .build()
+        Twitter.initialize(config)
+    }
+}
