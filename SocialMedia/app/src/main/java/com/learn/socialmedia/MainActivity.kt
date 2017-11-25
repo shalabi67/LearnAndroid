df33f718a7d46259ece303b3cfc6436b9175ce5e
mohammad <shalabi67@hotmail.com>
2017-11-25 20:45:07 +0100

    facebook share and like. share iamge is not working the buttton is disabled.

--- SocialMedia/app/src/main/java/com/learn/socialmedia/MainActivity.kt
+++ SocialMedia/app/src/main/java/com/learn/socialmedia/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.learn.socialmedia
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
@@ -9,8 +11,30 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import com.facebook.share.widget.LikeView
+import com.facebook.share.widget.ShareButton
+import com.facebook.share.model.ShareLinkContent
+import com.facebook.share.widget.ShareDialog
+import com.facebook.share.model.SharePhoto
+import com.facebook.share.model.ShareContent
+import com.facebook.share.model.SharePhotoContent
+import android.graphics.drawable.BitmapDrawable
+import com.learn.socialmedia.R.id.imageView
+
+
+
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -30,6 +54,56 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+
+        val facebookLikeView = findViewById<LikeView>(R.id.likeView) as LikeView
+        facebookLikeView.setLikeViewStyle(LikeView.Style.STANDARD)
+        facebookLikeView.setAuxiliaryViewPosition(LikeView.AuxiliaryViewPosition.INLINE)
+
+        facebookLikeView.setObjectIdAndType(
+                "https://www.facebook.com/AnimatedGreetingCards/",
+                LikeView.ObjectType.OPEN_GRAPH)
+
+
+        val shareButton = findViewById<ShareButton>(R.id.share_btn)
+        val content = ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://play.google.com/store/apps/details?id=com.ne3am.avatarproject"))
+                .setContentTitle("Animated greeting cards")
+                .setContentDescription("ebardy provides nice animated greeting cards.")
+                .build()
+        shareButton.shareContent = content
+
+
+        /*
+        val mediaShareButton = findViewById<ShareButton>(R.id.media_share_btn)
+
+        val imageView = findViewById<ImageView>(R.id.imageView2)
+        imageView.buildDrawingCache()
+        val bitmap = imageView.drawingCache
+        var imageContent = SharePhoto.Builder()
+               // .setImageUrl(Uri.parse("http://www.ebardy.com/ProjectImages/2014/a0634454-c618-4da8-a783-959f8c8b6e56/project.gif"))
+                .setBitmap(bitmap)
+                .build();
+        var imageContent1 = SharePhoto.Builder()
+                .setImageUrl(Uri.parse("http://www.ebardy.com/ProjectImages/2014/4d50bceb-1803-441b-b78f-afd3d6823d56/project.gif"))
+                .build();
+        val photoContent = SharePhotoContent.Builder()
+                //.setContentUrl(Uri.parse("https://play.google.com/store/apps/details?id=com.ne3am.avatarproject"))
+                .addPhoto(imageContent)
+               // .addPhoto(imageContent1)
+                .build()
+
+
+        mediaShareButton.shareContent = photoContent
+        */
+
+        /*
+        val sharedialog =  ShareDialog(this)
+        if (sharedialog.canShow(content)) {
+            ShareDialog.show(this, photoContent);
+        }
+        */
+
+
     }
 
     override fun onBackPressed() {
