198ee61e13b9d93ab7fcbbe73543f6d2d9fc5a6a
mohammad <shalabi67@hotmail.com>
2018-06-04 00:17:19 +0200

    async

---
+++ AndroidReactiveProgramming/app/src/main/java/com/learn/androidreactiveprogramming/NetworkingActivity.kt
@@ -0,0 +1,25 @@
+package com.learn.androidreactiveprogramming
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class NetworkingActivity : AppCompatActivity() {
+
+    val publishSubject =  NetworkService().createPublisher()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_networking)
+
+        getServiceData()
+    }
+
+    private fun getServiceData() {
+        NetworkService().getData()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe{ i -> Log.i("NETWORK", i.toString())}
+    }
+}
