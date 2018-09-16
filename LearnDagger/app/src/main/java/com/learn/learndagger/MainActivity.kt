750ce0c1a36632f659535893314a5f8dd5e91a54
mohammad <shalabi67@hotmail.com>
2018-09-16 11:59:30 +0200

    adding Dagger2 example

--- LearnDagger/app/src/main/java/com/learn/learndagger/MainActivity.kt
+++ LearnDagger/app/src/main/java/com/learn/learndagger/MainActivity.kt
@@ -2,18 +2,23 @@ package com.learn.learndagger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.learn.learndagger.configuration.DaggerMainComponent
-import com.learn.learndagger.domain.Body
+import com.learn.learndagger.domain.Person
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     @Inject
-    lateinit var body : Body
+    lateinit var person : Person
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         DaggerMainComponent.create().inject(this)
+
+        val textView  = findViewById<TextView>(R.id.message)
+        textView.setText(person.getBody().getHead())
     }
 }
