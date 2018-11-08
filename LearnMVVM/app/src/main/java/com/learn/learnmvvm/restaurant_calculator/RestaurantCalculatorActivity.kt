ac080361a2077133f19128251779d16969cbcffc
mohammad <shalabi67@hotmail.com>
2018-11-08 21:54:42 +0100

    some files not sure about them

--- LearnMVVM/app/src/main/java/com/learn/learnmvvm/restaurant_calculator/RestaurantCalculatorActivity.kt
+++ LearnMVVM/app/src/main/java/com/learn/learnmvvm/restaurant_calculator/RestaurantCalculatorActivity.kt
@@ -4,6 +4,8 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.learn.learnmvvm.R
 import com.learn.learnmvvm.databinding.ActivityRestaurantCalculatorBinding
 import com.learn.learnmvvm.restaurant_calculator.view_models.CalculatorViewModel
@@ -24,4 +26,17 @@ class RestaurantCalculatorActivity : AppCompatActivity() {
             this.vm = calculatorViewModel
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.activity_resturant_calculator, menu);
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
 }
